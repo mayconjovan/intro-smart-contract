e6dd67ab043567c49b3644b4982caaf4bfcba75e
Maycon J Pereira <mayconjovan@gmail.com>
2024-05-08 20:41:39 -0300

    add age

--- learn-solidity/ExternalManipulateValue.sol
+++ learn-solidity/ExternalManipulateValue.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.0;
 
 contract ExternalManipulateValues {
     string public name;
+    uint public age;
 
     function setName(string memory _name) public {
         name = _name;
     }
+
+    function setAge(uint _age) public {
+        age = _age;
+    }
+
 }
